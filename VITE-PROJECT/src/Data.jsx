32a208876b2ca9063b9c0fa444984b169e6dc5c6
Justin Ram <justramin000@gmail.com>
2025-01-17 13:32:20 +0530

    first commit

---
+++ VITE-PROJECT/src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name:"Arshad",
+        email:"arshad@gmail.com",
+        id:1
+    },
+    {
+        name:"Alan",
+        email:"alan@gmail.com",
+        id:2
+    },
+    {
+        name:"Pranav",
+        email:"pranav@gmail.com",
+        id:3
+    }
+]
